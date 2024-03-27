090136c20354b8a6c11aa30ab6e607dab8ddef1b
Furqan Ahmad <146337498+bahriaFurqan@users.noreply.github.com>
2024-03-27 22:07:17 +0500

    Update room.h

--- room.h
+++ room.h
@@ -1,23 +1,15 @@
-class Room {
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+#include "course.h"
+class room {
 public:
-    string R_No;
-    vector<Course> courses;
-
-    void addCourse(const Course& c) {
-        courses.push_back(c);
+    string room_no;
+    vector<course> courses; // Courses assigned to the room
+    room(string n) {
+        room_no = n;
     }
-
-    void display() const {
-        cout << "Room Number: " << R_No << endl;
-        cout << "Courses:\n";
-        for (const auto& course : courses) {
-            cout << "Course: " << course.name << " (Time: " << course.time << ") "
-                << "Teacher: " << course.teacher.name << endl;
-            cout << "Students:\n";
-            for (const auto& student : course.students) {
-                student.display();
-                cout << endl;
-            }
-        }
-    }
 };
+
